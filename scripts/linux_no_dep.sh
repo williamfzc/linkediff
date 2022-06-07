9e3bad18f17cfd965151de8995d8310562870469
williamfzc <178894043@qq.com>
2022-06-08 00:06:32 +0800

    chore: simple script

--- scripts/linux_no_dep.sh
+++ scripts/linux_no_dep.sh
@@ -1,7 +1,5 @@
 set -e
 
-PATCH_FILE="diff.patch"
-
 # python
 python3 --version
 pip3 install --upgrade linkediff
@@ -12,10 +10,6 @@ mv ./coca_linux ./coca
 chmod +x ./coca
 ./coca version
 
-# diff
-git status
-git diff HEAD~3 HEAD > ${PATCH_FILE}
-
 # run
-linkediff init --patch_file=./${PATCH_FILE} --to_xmind=./result.xmind --to_json=./result.json --coca_cmd=./coca
+linkediff init --to_xmind=./result.xmind --to_json=./result.json --coca_cmd=./coca
 linkediff run
